9e490a738d9ce64a1d1ca677fecd4b11998b3dd8
Svetlanagrosu <grosu.svetlana.miha@gmail.com>
2024-03-14 22:52:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//for(let i = 0; i < 10; i++) {
+    //console.log(i,`Au`)
+//}
+
+//for (let i = 10; i >=1; i--) console.log(`Au`, i)
+
+//for (let i = 10; i >=1; i-=2) console.log(`Au`, i)
+
+//let i = 10 
+
+//while (i >= 0) {
+   // console.log(`Au`, i) 
+//}
+
+let i = 10
+
+while (--i) {
+    console.log(`Au`, i)
+    
+}
